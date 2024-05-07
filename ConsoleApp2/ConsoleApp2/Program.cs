c6e44c8fc775843c6f89c159f8a100a35f3edf96
Ørjan Malde <red@foxi.me>
2024-05-07 09:56:43 +0200

    315A. guess the secret.
    Signed-off-by: Ørjan Malde <red@foxi.me>

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace ConsoleApp2;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        var random = new Random();
+        var secret = random.Next(1,100);
+        int guessesUsed = 0, guess;
+        Console.WriteLine("Guess the number between 1 and 100.");
+        while (true)
+        {
+            string guessStr = Console.ReadLine();
+            if (string.IsNullOrEmpty(guessStr) || string.IsNullOrWhiteSpace(guessStr))
+                continue;
+            try
+            {
+                guess = int.Parse(guessStr);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Not a number...");
+                continue;
+            }
+            if (guess > secret)
+            {
+                Console.WriteLine("Guess is greater than the secret.");
+                guessesUsed++;
+            }
+            else if (guess < secret)
+            {
+                Console.WriteLine("Guess is lesser than the secret.");
+                guessesUsed++;
+            }
+            else
+            {
+                Console.WriteLine("Congratulations, you won.");
+                Console.WriteLine($"You guessed {guessesUsed} times.");
+                break;
+            }
+        }
+    }
+}
